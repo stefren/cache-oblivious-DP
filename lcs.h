6adecc34d92bba915580ba399ec0201c145b3014
Stef Ren <stefren@mit.edu>
2017-11-16 17:12:44 -0500

    changing types

--- lcs.h
+++ lcs.h
@@ -2,15 +2,15 @@
 
 /* Represents an input x, y pair for LCS */
 typedef struct {
-  int x_len;
-  int y_len;
+  uint64_t x_len;
+  uint64_t y_len;
   char* x;
   char* y;
 } lcs_input_t;
 
 typedef struct {
   char* start;
-  int length;
+  uint64_t length;
 } boundary_t; 
 
 typedef struct {
@@ -18,11 +18,18 @@ typedef struct {
   boundary_t* right;
 } boundary_result_t;
 
-typedef struct {
-  char* buf;
-  int capacity;
-  int length;
-} lcs_result_t;
+struct lcs_result_node_t {
+  char character;
+  struct lcs_result_node_t* next;
+}; 
+typedef struct lcs_result_node_t lcs_result_node_t;
+
+struct lcs_result_t {
+  lcs_result_node_t* head;
+  lcs_result_node_t* tail;
+  uint64_t size;
+};
+typedef struct lcs_result_t lcs_result_t;
 
 void lcs_regular(lcs_input_t* input);
 void lcs_cache_oblivious(lcs_input_t* input);
@@ -30,7 +37,7 @@ void lcs_cache_oblivious(lcs_input_t* input);
 /* Return the character at indices i, j given a matrix stored as a contiguous
  * block of memory in row-major order.
  */
-static inline char get_char_at_indices(char* dp_matrix, int row_size, int i, int j) {
+static uint64_t get_char_at_indices(uint64_t* dp_matrix, int row_size, int i, int j) {
   if (j < 0 || i < 0 || i > row_size) {
     return 0;
   }
@@ -40,8 +47,10 @@ static inline char get_char_at_indices(char* dp_matrix, int row_size, int i, int
 /* Set the character at indices i, j given a matrix stored as a contiguous
  * block of memory in row-major order.
  */
-static inline char set_char_at_indices(char* dp_matrix, int row_size, int i, int j, char value) {
+static uint64_t set_char_at_indices(uint64_t* dp_matrix, int row_size, int i, int j, char value) {
+  if (j < 0 || i < 0 || i > row_size) {
+    return -1;
+  }
   dp_matrix[j*row_size + i] = value;
   return 0;   // success
 }
-
