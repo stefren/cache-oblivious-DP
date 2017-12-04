b7ac5f8bd2c8a2963dc2fa441f0fa0edec263421
Stef Ren <stefren@mit.edu>
2017-12-03 23:10:57 -0500

    Fix naive algorithm

--- floyd_warshall/floyd_warshall_naive.c
+++ floyd_warshall/floyd_warshall_naive.c
@@ -7,9 +7,9 @@
 
 /* Naive implementation of Floyd-Warshall */
 void floyd_warshall_naive(dp_matrix_t* X) {
-  for (uint64_t i = 0; i < X->width; i++) {
-    for (uint64_t j = 0; j < X->width; j++) {
-      for (uint64_t k = 0; k < X->width; k++) {
+  for (uint64_t k = 0; k < X->width; k++) {
+    for (uint64_t i = 0; i < X->width; i++) {
+      for (uint64_t j = 0; j < X->width; j++) {
         if (get_entry(X, i, k) != -1 && get_entry(X, k, j) != -1 && 
             get_entry(X, i, k) + get_entry(X, k, j) < get_entry(X, i, j)) {
           set_entry(X, i, j, get_entry(X, i, k) + get_entry(X, k, j));
