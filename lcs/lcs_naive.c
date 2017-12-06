5ae4a06f8120b457334e39a610d327990f183867
Stef Ren <stefren@mit.edu>
2017-12-05 22:33:45 -0500

    Fix naive LCS

--- lcs/lcs_naive.c
+++ lcs/lcs_naive.c
@@ -3,11 +3,9 @@
 #include <string.h>
 #include "./lcs.h"
 
-void backtrack(lcs_input_t* input, lcs_result_t* result, uint64_t* dp, int64_t i, int64_t j);
+void backtrack(dp_matrix_t* X, lcs_input_t* input, lcs_result_t* result, uint64_t i, uint64_t j);
 
 void lcs_naive(dp_matrix_t* X, lcs_input_t* input) {
-  uint64_t width = X->width;
-  uint64_t height = X->height;
   uint64_t i, j;
   for (i = 0; i < X->width; i++) {
     for (j = 0; j < X->height; j++) {
@@ -20,40 +18,41 @@ void lcs_naive(dp_matrix_t* X, lcs_input_t* input) {
       }
     }
   }
-  printf("Longest subsequence: %llu\n", get_entry(X, width - 1, height - 1));
-//  lcs_result_t* result = (lcs_result_t*) malloc(sizeof(lcs_result_t);
-//  backtrack(input, &result, dp, width-1, height-1);
-//  printf("Longest subsequence: %llu\n", result.size);
-//  printf("\n");
-//  for (lcs_result_node_t* node = result.head; node != NULL; node = node->next) {
-//    printf("%c", node->character);
-//  }
-//  printf("\n");
-//  free(dp);
-//  free_lcs_result(&result);
+  lcs_result_t* result = (lcs_result_t*) malloc(sizeof(lcs_result_t));
+  result->size = 0;
+  result->head = NULL;
+  result->tail = NULL;
+  backtrack(X, input, result, X->width-1, X->height-1);
+  printf("Longest subsequence: %llu\n", result->size);
+  for (lcs_result_node_t* node = result->head; node != NULL; node = node->next) {
+    printf("%c", node->character);
+  }
+  printf("\n");
+  free_lcs_result(result);
 }
 
-// void backtrack(dp_matrix_t* X, lcs_result_t* result, uint64_t* dp, int64_t i, int64_t j) {
-//   if (i < 0 || j < 0) {
-//     return;
-//   }
-//   if (X == ) {
-//     backtrack(input, result, dp, i-1, j-1);
-//     lcs_result_node_t* new_node = malloc(sizeof(lcs_result_node_t));
-//     new_node->character = input->x[i];
-//     new_node->next = NULL;
-//     if (result->head == NULL) {
-//       result->head = new_node;
-//       result->tail = new_node;
-//       result->size = 1;
-//     } else {
-//       result->tail->next = new_node;
-//       result->tail = new_node;
-//       result->size++;
-//     }
-//   } else if (get_entry(X, i, j-1) > get_char_at_indices(X, i-1, j)) {
-//     backtrack(input, result, dp, i, j-1);
-//   } else {
-//     backtrack(input, result, dp, i-1, j);
-//   }
-// }
+ void backtrack(dp_matrix_t* X, lcs_input_t* input, lcs_result_t* result, uint64_t i, uint64_t j) {
+   if (i == -1 || j == -1) {
+     return;
+   }
+   if (input->x[i] == input->y[j]) {
+     printf("%c", input->x[i]);
+     backtrack(X, input, result, i-1, j-1);
+     lcs_result_node_t* new_node = malloc(sizeof(lcs_result_node_t));
+     new_node->character = input->x[i];
+     new_node->next = NULL;
+     if (result->head == NULL) {
+       result->head = new_node;
+       result->tail = new_node;
+       result->size = 1;
+     } else {
+       result->tail->next = new_node;
+       result->tail = new_node;
+       result->size++;
+     }
+   } else if (get_entry(X, i, j-1) > get_entry(X, i-1, j)) {
+     backtrack(X, input, result, i, j-1);
+   } else {
+     backtrack(X, input, result, i-1, j);
+	 }
+ }
